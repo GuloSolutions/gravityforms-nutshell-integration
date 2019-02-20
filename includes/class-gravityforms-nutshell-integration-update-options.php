4b891aea967c8fa3c85204867bfc93c7b8266f64
radboris <rad.borislavov@gmail.com>
2019-02-20 14:55:12 -0600

    remvoe class reference

--- includes/class-gravityforms-nutshell-integration-update-options.php
+++ includes/class-gravityforms-nutshell-integration-update-options.php
@@ -1,12 +1,12 @@
 <?php
 
-use Controllers\GravityFormsDataController;
+use Controllers\GravityFormsController;
 
 include_once('/home/radboris/gravityforms-nutshell-integration/app/Controllers/GravityFormsDataController.php');
 
 const GRAVITYFORMS_NUTSHELL_PLUGIN_SETTINGS_PAGE = '/wp-admin/options-general.php?page=my-setting-admin';
 
-if (!empty($_POST)) {
+if (!empty($_POST) && isset($_POST['checkbox'])) {
     $all_options = [];
     $unchecked = [];
 
@@ -22,13 +22,11 @@ if (!empty($_POST)) {
         }
     }
 
-    error_log(print_r('notes', true));
-
-    error_log(print_r($_POST['note'], true));
-
-
-
     if (isset($_POST['checkbox'])) {
+        error_log(print_r('this is user', true));
+
+        error_log(print_r($_POST['nutshell'], true));
+
         $check_values = array_keys($_POST['checkbox']);
         $unchecked = array_diff($all_options, $check_values);
 
@@ -39,11 +37,6 @@ if (!empty($_POST)) {
         foreach ($unchecked as $u) {
             update_option($u, 0);
         }
-
-        $form = new GravityFormsDataController();
-        $form->sendData();
-
-        error_log(print_r($form->sendData(), true));
 
         header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . GRAVITYFORMS_NUTSHELL_PLUGIN_SETTINGS_PAGE);
         exit;
